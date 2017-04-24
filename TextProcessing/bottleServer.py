6fac49611c2b7f08f3819d6d9fe7720d9d9fdd65
Mitesh Patel <patel221881@tamu.edu>
2017-04-23 21:35:44 -0500

    need to change all the localhosts to the ip of the server

--- TextProcessing/bottleServer.py
+++ TextProcessing/bottleServer.py
@@ -1,22 +1,13 @@
 from bottle import route, run, template
 from datafetch import createURL
 
-@route('/hello/<name>')
-def index(name):
-	return template('<b>Hello {{name}}</b>!', name=name)
-
 @route('/')
 def mainPage():
-	return template('<b>You are connected to the server {{name}}</b>', name = 'Muhammad');
+	return template('You have connected to the server')
 
-@route('/string')
-def requestString():
-	print('request for strig')
-	return "You have requested the string"
-
-@route('/request/<city>/<zip>/<bed>/<bath>')
-def getHouse(city, zipC, bed, bath):
+@route('/request/<city>/<zipC>/<bed>/<bath>')
+def getHouses(city, zipC, bed, bath):
 	return createURL(city,bed,bath,zipC)
-	#return template('You are requesting a house in {{city}}, {{zip}}. With {{bed}} bedrooms and {{bath}} baths.', city = city, zip = zipC, bed = bed, bath = bath)
 
-run(host='localhost', port=80)
+
+run(host='localhost', port=8080)
